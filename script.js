fef18f6db2c33205c029a8b0179bf3505c87e35d
Pavel Asafov <pavelaof@gmail.com>
2023-11-22 16:31:59 +0600

    cosmetic change

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var dropdownHeader = document.querySelector('.dropdown-header');
-  let selectTitle = dropdownHeader.querySelector('#selected-item')
-  let iconTitle = dropdownHeader.querySelector('.icon')
-  var dropdownBody = document.querySelector('.dropdown-body');
-  let listOfItems = dropdownBody.querySelectorAll('.dropdown-item')
+  let dropDown = document.querySelector('.custom-dropdown')
+  let dropdownHeader = document.querySelector('.dropdown-header');
+  let textTitle = dropdownHeader.querySelector('#selected-item');
+  let iconTitle = dropdownHeader.querySelector('.icon');
+  let iconArrow = dropdownHeader.querySelector('.icon-arrow');
+  let dropdownBody = document.querySelector('.dropdown-body');
+  let listOfItems = dropdownBody.querySelectorAll('.dropdown-item');
 
-  dropdownHeader.addEventListener('click', function () {
-    let displayOn = dropdownBody.style.display === 'block'
+  dropDown.addEventListener('click', function () {
+    let displayNone = dropdownBody.style.display === 'none'
 
-    dropdownBody.style.display = displayOn ? 'none' : 'block';
-  });
+    if (displayNone) {
+      dropdownBody.style.display = 'block';
+      iconArrow.classList.add('icon-arrow-rotated')
+    } else {
+      dropdownBody.style.display = 'none';
+      iconArrow.classList.remove('icon-arrow-rotated')
+
+      listOfItems.forEach((item) => {
+        if (item.classList.contains('dropdown-item-selected')) {
+          item.classList.remove('dropdown-item-selected')
+        } else if (item.textContent.trim() === textTitle.textContent) {
+          item.classList.add('dropdown-item-selected')
+        }
+      })
+    }
+  })
 
   listOfItems.forEach(function (item) {
     item.addEventListener('click', function () {
-      selectTitle.textContent = item.textContent.trim();
+      let arrayIconClasses = iconTitle.classList
+      let [, classSecond] = item.querySelector('.icon').classList
 
-      let [, classSecond] =
-        item.querySelector('.icon').className.split(' ')
-
+      arrayIconClasses.forEach(classItem => iconTitle.classList.remove(classItem))
+      iconTitle.classList.add('icon')
       iconTitle.classList.add(classSecond)
-    });
-  });
+      textTitle.textContent = item.textContent.trim();
+    })
+  })
 
   listOfItems[1].click();
 });
